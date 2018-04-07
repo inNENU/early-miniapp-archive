202d19ce493bd47ed45489bdcdfabd0ea4a0216c
MrHope <zhangbowang1998@gmail.com>
2018-04-07 23:54:01 +0800

    初步构建wechat夜间主题

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -10,15 +10,3 @@ Page({
   onShow() { this.setData({ T: a.T, nm: a.nm, page: u.sP(this.data.page, a) }) },
   onPageScroll(e) { let p = u.nav(e, this.data.page); if (p) { this.setData({ page: p }) } },
 })
-  // < button bindtap= 'goOther' > 新生报到 < /button>
-  //   < button bindtap= 'goDorm' > 寝室 < /button>
-  //     < button bindtap= 'goLife' > 生活 < /button>
-  //       < button bindtap= 'goOther' > 食堂 < /button>
-  //         < button bindtap= 'goOther' > 学习 < /button>
-  //           < button bindtap= 'goOther' > 校园网 < /button>
-  //             < button bindtap= 'goCard' > 校园卡 < /button>
-  //               < button bindtap= 'goOther' > 学生组织 < /button>
-  //                 < button bindtap= 'goOther' > 校区位置 < /button>
-  //                   < button bindtap= 'goOther' > 资助 < /button>
-  //                     < button bindtap= 'goOther' > 吃喝玩乐 < /button>
-  //                       < button bindtap= 'goOther' > 长春气候 < /button>
