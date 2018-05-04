f34d05e2ef8ffda8714d5a90ac2752926d1b5e5f
Mr.Hope <zhangbowang1998@gmail.com>
2018-05-05 00:18:04 +0800

    修复餐厅显示异常的问题

--- pages/guide/dining/details/dining6.js
+++ pages/guide/dining/details/dining6.js
@@ -7,6 +7,7 @@ Page({
       { name: 'img', src: 'https://pic.kuaizhan.com/g2/M01/80/FC/wKjmqll8TAeAJSd_AACUpE4nxHc7211506/imageView/v1/thumbnail/640x0' },
       { name: 'img', src: 'https://pic.kuaizhan.com/g2/M00/80/FC/wKjmqll8TAqAN1CDAAGCsn0h8F48867427/imageView/v1/thumbnail/640x0' },
       { name: 'img', src: 'https://pic.kuaizhan.com/g2/M00/7F/40/CgpQVFl8TAyAMlO2AADkUEmv9nA3724359/imageView/v1/thumbnail/640x0' },
+      { name: 'foot' },
     ],
   },
   onLoad(e) { let page = u.sP(this.data.page, a, e); this.setData({ T: a.T, nm: a.nm, page: page }) },
--- pages/guide/dorm/dorm.js
+++ pages/guide/dorm/dorm.js
@@ -8,7 +8,7 @@ Page({
       { name: 'p', head: '禁用物品', text: '宿舍内严禁使用违章电器，如电熨斗、电吹风、电炉、电水壶、电热毯、热得快等大功率电器。床帘和吊椅也是不允许的。(只可以挂蚊帐)' },
       { name: 'p', text: '校会君偷偷告诉萌新们一句：元旦的时候男女生可以互串寝室哦~' },
       { name: 'foot' },
-    ], grey: true,
+    ], 
   },
   onLoad(e) { this.setData({ T: a.T, nm: a.nm, page: u.sP(this.data.page, a, e) }) }, onPageScroll(e) { let p = u.nav(e, this.data.page); if (p) { this.setData({ page: p }) } }, img(e) { let p = u.img(this.data.page, e); this.setData({ page: p }) }, back() { u.back() },
 })
