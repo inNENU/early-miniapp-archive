510f5d389d135ad678387965399fbfa6d80583e6
Mr.Hope <zhangbowang1998@gmail.com>
2018-05-10 00:12:33 +0800

    资助界面线上化

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -3,7 +3,7 @@ Page({
   data: {
     page: [
       { tag: 'head', title: '东师指南', top: true, aimStep: 1, grey: true },
-      { tag: 'list', content: [{ text: '新生报到', url: 'check/check' }, { text: '寝室', url: 'dorm/dorm', aim: 'dorm' }, { text: '生活', url: 'life/life', aim: 'life' }, { text: '食堂', url: 'dining/dining', aim: 'dining' }, { text: '校园卡', url: 'card/card' }, { text: '学习', url: 'study/study', aim: 'study' }, { text: '资助', url: 'subsidize/subsidize' }, { text: '资助2', url: 'subsidize/subsidize2' },] },
+      { tag: 'list', content: [{ text: '新生报到', url: 'check/check' }, { text: '寝室', aim: 'dorm' }, { text: '生活', aim: 'life' }, { text: '食堂', aim: 'dining' }, { text: '校园卡', url: 'card/card' }, { text: '学习', aim: 'study' }, { text: '资助', aim: 'subsidize' },] },
     ],
   },
   onShow() { u.sP(this.data.page, this, a) },
