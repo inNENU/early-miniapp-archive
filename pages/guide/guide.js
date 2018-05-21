052c8f270e66e7522523c04d39ad007bf0fadefa
Mr.Hope <zhangbowang1998@gmail.com>
2018-05-21 17:22:58 +0800

    icon修复

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -3,7 +3,17 @@ Page({
   data: {
     page: [
       { tag: 'head', title: '东师指南', top: true, aimStep: 1, grey: true },
-      { tag: 'list', content: [{ text: '新生报到', aim: 'check' }, { text: '寝室', aim: 'dorm' }, { text: '生活', aim: 'life' }, { text: '食堂', icon: '/icon/dining.svg', aim: 'dining' }, { text: '校园卡', icon: '/icon/card.svg', url: 'card/card' }, { text: '学习', aim: 'study' }, { text: '资助', aim: 'subsidize' },] },
+      {
+        tag: 'list', content: [
+          { text: '新生报到', icon: '/icon/check.svg', aim: 'check' },
+          { text: '寝室', icon: '/icon/dorm.svg', aim: 'dorm' },
+          { text: '生活', icon: '/icon/life.svg', aim: 'life' },
+          { text: '食堂', icon: '/icon/dining.svg', aim: 'dining' },
+          { text: '校园卡', icon: '/icon/card.svg', url: 'card/card' },
+          { text: '学习', aim: 'study' },
+          { text: '资助', aim: 'subsidize' },
+        ]
+      },
     ],
   },
   onLoad() { u.sP(this.data.page, this, a); u.cRU(); },
