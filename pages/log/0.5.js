93f337973db817e2058e7b86f60e8f31698c57f8
Mr.Hope <zhangbowang1998@gmail.com>
2018-05-24 09:22:58 +0800

    创建login界面

--- pages/log/0.5.js
+++ pages/log/0.5.js
@@ -3,7 +3,8 @@ Page({
   data: {
     page: [
       { tag: 'head', title: 'V0.5开发日志', grey: true },
-      { tag: 'p', head: 'V0.5.1', text: '构建了出现错误显示的界面；' },
+      { tag: 'p', head: 'V0.5.1', text: '构建了出现错误显示的界面；\n改进了grid的显示效果；\n改进了微信主题下的分割线宽度；' },
+      { tag: 'p', head: 'V0.5.2', text: '创建了login界面并进行了初步设计；' },
     ],
   },
   onLoad(e) { u.sP(this.data.page, this, a, e) }, onPageScroll(e) { u.nav(e, this) }, back() { u.back() },
---
+++ pages/settings/login.js
@@ -0,0 +1,33 @@
+Page({
+
+  data: {
+    loginAnimation: undefined
+  },
+  input(e) {
+    console.log(e)
+  },
+  onShow() {
+    var animation = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    console.log(this);
+    console.log(this.animation)
+    animation.rotate(180).step()
+    animation.rotate(180).step()
+    console.log(this.animation)
+    this.setData({
+      loginAnimation: animation.export()
+    })
+    // // for (let i = 0; i < 30; i++) {
+    // setTimeout(function () {
+    //   animation.rotate(180).step()
+    //   this.setData({
+    //     loginAnimation: animation.export()
+    //   })
+    // }.bind(this), 20505)
+    // // }
+  },
+})
