8fe9a4ada3ce63b6327b198f20b80c36dce2b0a0
Mr.Hope <zhangbowang1998@gmail.com>
2019-04-10 20:52:48 +0800

    为播放器添加歌曲列表

--- function/player.js
+++ function/player.js
@@ -8,7 +8,9 @@ $page('music', {
   data: {
     canplay: false,
     currentTime: 0,
-    songLength: 1
+    songLength: 1,
+    currentSong: {},
+    songListDisplay: false
   },
   onNavigate() {
     let currentSong;
@@ -16,31 +18,38 @@ $page('music', {
       songList = $file.readJson('function/song'),
       mode = wx.getStorageSync('playMode');
 
+    // 写入基本信息
     this.data.index = index;
     this.data.info = a.info;
     this.data.nm = a.nm;
     this.data.play = a.music.play;
     this.data.mode = mode ? mode : (wx.setStorageSync('playMode', 0), 0);
+    this.share = false;
+
     if (songList) {
+
+      // 写入歌曲列表与当前歌曲信息
       currentSong = songList[index];
       this.data.songList = songList;
-      this.data.title = currentSong.title;
-      this.data.singer = currentSong.singer;
-      this.data.cover = currentSong.cover;
+      this.data.currentSong = currentSong;
+
       this.set = true;
     } else
       $set.request('function/song', data => {
+
+        // 写入歌曲列表与当前歌曲信息
         currentSong = data[index];
         this.data.songList = data;
-        this.data.title = currentSong.title;
-        this.data.singer = currentSong.singer;
-        this.data.cover = currentSong.cover;
+        this.data.currentSong = currentSong;
+
         this.set = true;
+
+        // 写入JSON文件
         $file.writeJson('function', 'song', data);
       });
 
   },
-  onLoad(e) {
+  onLoad(option) {
 
     // 加载字体
     wx.loadFontFace({
@@ -53,60 +62,70 @@ $page('music', {
     let currentSong;
 
     if (!this.set) {
-      const index = e.index ? (a.music.index = e.index, e.index) : a.music.index,
+      if (option.index) a.music.index = option.index;
+
+      const { index } = a.music,
         songList = $file.readJson('function/song'),
         mode = wx.getStorageSync('playMode');
 
+      // 写入基本信息
       this.setData({
-        share: e.share ? e.share : false,
+        share: option.share ? option.share : false,
         index,
         info: a.info,
         nm: a.nm,
         play: a.music.play,
         mode: mode ? mode : (wx.setStorageSync('playMode', 0), 0)
       });
+
+
       if (songList) {
+        // 写入歌曲列表与当前歌曲信息
         currentSong = songList[index];
-        this.setData({
-          songList,
-          title: currentSong.title,
-          singer: currentSong.singer,
-          cover: currentSong.cover
-        });
+        this.setData({ songList, currentSong });
+
+        // 如果正在播放，设置能够播放
         if (a.music.played) this.setData({ canplay: true });
+
+        // 对音频管理器进行设置
         else {
+          manager.epname = 'NenuYouth';
           manager.src = currentSong.src;
           manager.title = currentSong.title;
-          manager.epname = 'NenuYouth';
           manager.singer = currentSong.singer;
           manager.coverImgUrl = currentSong.cover;
         }
-      } else
-        $set.request('function/song', data => {
-          currentSong = data[index];
-          this.setData({
-            songList: data, title: currentSong.title,
-            singer: currentSong.singer,
-            cover: currentSong.cover
-          });
-          if (a.music.played) this.setData({ canplay: true });
-          else {
-            manager.src = currentSong.src;
-            manager.title = currentSong.title;
-            manager.epname = 'NenuYouth';
-            manager.singer = currentSong.singer;
-            manager.coverImgUrl = currentSong.cover;
-          }
-          $file.writeJson('function', 'song', data);
-        }, this);
+
+        // 在线获取歌曲列表
+      } else $set.request('function/song', data => {
+        currentSong = data[index];
+        this.setData({ songList: data, currentSong });
+
+        // 如果正在播放，设置能够播放
+        if (a.music.played) this.setData({ canplay: true });
+
+        // 对音频管理器进行设置
+        else {
+          manager.epname = 'NenuYouth';
+          manager.src = currentSong.src;
+          manager.title = currentSong.title;
+          manager.singer = currentSong.singer;
+          manager.coverImgUrl = currentSong.cover;
+        }
+
+        // 写入JSON文件
+        $file.writeJson('function', 'song', data);
+      });
 
     }
 
+    //　能够播放100ms后设置可以播放
     manager.onCanplay(setTimeout(() => {
       console.log('Canplay'); // 调试
       this.setData({ canplay: true });
     }), 100);
 
+    // 在相应动作时改变状态
     manager.onPlay(() => {
       this.setData({ play: true });
       a.music.play = true;
@@ -119,13 +138,16 @@ $page('music', {
 
     manager.onTimeUpdate(() => {
 
-      // 调试
-      console.log(`TimeUpdate,currentTime是${manager.currentTime},
-      bufferedTime是${manager.buffered},duration是${manager.duration}`);
+      /*
+       * 调试
+       * console.log(`TimeUpdate,currentTime是${manager.currentTime}`);
+       * console.log(`bufferedTime是${manager.buffered},duration是${manager.duration}`);
+       */
 
       const presentSecond = parseInt(manager.currentTime % 60).toString(),
         totalSecond = parseInt(manager.duration % 60).toString();
 
+      // 更新歌曲信息
       this.setData({
         songLength: parseInt(manager.duration * 100) / 100,
         total: [
@@ -141,19 +163,23 @@ $page('music', {
         canplay: true
       });
 
+      // 设置播放状态
       a.music.played = true;
     });
+
+    // 缓冲中
     manager.onWaiting(() => {
       console.warn('waiting');
       this.setData({ canplay: false });
     });
 
-    manager.onEnded(() => {
-      this.next();
-    });
-
     manager.onPrev(() => {
       this.previous();
+    });
+
+    // 歌曲播放结束
+    manager.onEnded(() => {
+      this.end();
     });
 
     manager.onNext(() => {
@@ -191,51 +217,100 @@ $page('music', {
       console.log(e.detail.value); // 调试
     }
   },
-  next() {
-    let { index } = this.data,
-      j;
-    const { length: total } = this.data.songList;
+  end() {
+    let { index } = this.data;
+    const total = this.data.songList.length;
 
     switch (this.data.mode) {
       case 3:
-        do j = Math.round(Math.random() * total - 0.5); while (i === j);
-        index = j;
+        let temp;
+
+        do temp = Math.round(Math.random() * total - 0.5); while (index === temp);
+        index = temp;
         break;
+      case 2:
+        index = index + 1 === total ? 'stop' : index + 1;
+        wx.showToast({ title: '播放完毕', icon: 'none' });
+      case 1:
+        break;
+      case 0:
+      default: index = index + 1 === total ? 0 : index + 1;
+    }
+    this.Switch(index);
+  },
+  next() {
+    let { index } = this.data;
+    const total = this.data.songList.length;
+
+    switch (this.data.mode) {
+      case 3:
+        let temp;
+
+        do temp = Math.round(Math.random() * total - 0.5); while (index === temp);
+        index = temp;
+        break;
+      case 2:
+        if (index + 1 === total) {
+          index = 'nothing';
+          wx.showToast({ title: '已是最后一曲', icon: 'none' });
+        } else index += 1;
+        break;
+      case 1:
+      case 0:
       default: index = index + 1 === total ? 0 : index + 1;
     }
     this.Switch(index);
   },
   previous() {
-    let { index } = this.data,
-      j;
+    let { index } = this.data;
     const { length: total } = this.data.songList;
 
     switch (this.data.mode) {
       case 3:
-        do j = Math.round(Math.random() * total - 0.5); while (i === j);
-        index = j;
+        let temp;
+
+        do temp = Math.round(Math.random() * total - 0.5); while (index === temp);
+        index = temp;
         break;
+      case 2:
+        if (index === 0) {
+          index = 'nothing';
+          wx.showToast({ title: '已是第一曲', icon: 'none' });
+        } else index -= 1;
+        break;
+      case 1:
+      case 0:
       default: index = index === 0 ? total - 1 : index - 1;
     }
-    console.log(index);
     this.Switch(index);
   },
   Switch(index) {
-    const currentSong = this.data.songList[index];
+    if (index === 'stop') {
 
-    this.setData({
-      index,
-      title: currentSong.title,
-      singer: currentSong.singer,
-      cover: currentSong.cover,
-      play: false,
-      canPlay: false
-    });
-    manager.src = currentSong.src;
-    manager.title = currentSong.title;
-    manager.singer = currentSong.singer;
-    manager.coverImgUrl = currentSong.cover;
-    a.music.index = index;
+      this.setData({
+        play: false,
+        canPlay: false
+      });
+
+      manager.stop();
+
+    } else if (index !== 'nothing') {　// 正常赋值
+
+      const currentSong = this.data.songList[index];
+
+      this.setData({
+        index,
+        currentSong,
+        play: false,
+        canPlay: false
+      });
+
+      manager.src = currentSong.src;
+      manager.title = currentSong.title;
+      manager.singer = currentSong.singer;
+      manager.coverImgUrl = currentSong.cover;
+      a.music.index = index;
+    }
   },
   cA(e) {
     $set.component(e, this);
@@ -260,7 +335,23 @@ $page('music', {
     wx.showToast({ title: `${modeName}模式`, icon: 'none' });
   },
   list() {
-    wx.showToast({ title: '开发中', 'icon': 'none' });// 待完善
+    this.setData({ songListDisplay: !this.data.songListDisplay });
+    /*
+     * Comment
+     * if (this.data.songListDisplay) {
+     *   this.setData({ hide: true });
+     *   setTimeout(() => {
+     *     this.setData({ songListDisplay: false });
+     *   }, 500);
+     * } else this.setData({
+     *   songListDisplay: true,
+     *   hide: false
+     * });
+     */
+  },
+  change(res) {
+    this.list();
+    this.Switch(res.currentTarget.dataset.index);
   },
   onShareAppMessage() {
     return {
